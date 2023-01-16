4c4b825556b016cd4179916e7174ed89d241bd2b
Dian Abdul Syahid <4bdulsyahid@gmail.com>
2023-01-16 18:15:57 +0700

    Initializing...

--- js/engine.js
+++ js/engine.js
@@ -1,4 +1,16 @@
 
 $(document).ready(function() {
-  
+    // Preload
+    if (
+        $(window).load(function() {
+          $('body').addClass('overflowhidden');
+          setTimeout(function() { 
+            $("#preloader").fadeOut("slow", function() {
+              $(this).remove();
+            });
+            $('body').removeClass('overflowhidden');
+        }, 5000);
+        })
+      ); 
+      
 });
