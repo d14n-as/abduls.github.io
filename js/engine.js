669efdfda6563246daedd3c9412b30966b6de8a8
d14n-as.github.io <4bdulsyahid@gmail.com>
2023-04-01 20:09:13 +0700

    Changes navbar on mobile version

--- js/engine.js
+++ js/engine.js
@@ -1,15 +1,15 @@
 
-$(document).ready(function() {
-    if (
-        $(window).load(function() {
-          $('body').addClass('overflowhidden');
-          setTimeout(function() { 
-            $("#preloader").fadeOut("slow", function() {
-              $(this).remove();
-            });
-            $('body').removeClass('overflowhidden');
-        }, 5000);
-        })
-      ); 
+// $(document).ready(function() {
+//     if (
+//         $(window).load(function() {
+//           $('body').addClass('overflowhidden');
+//           setTimeout(function() { 
+//             $("#preloader").fadeOut("slow", function() {
+//               $(this).remove();
+//             });
+//             $('body').removeClass('overflowhidden');
+//         }, 5000);
+//         })
+//       ); 
       
-});
+// });
